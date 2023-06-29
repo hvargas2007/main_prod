f3abb73692785eb65ed70bd4a07390e0d435f484
vargascloudh <hermes.vargas@cloudhesive.comgit config --global user.name vargascloudh>
2023-06-29 18:10:56 -0300

    vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create a vpc
 module "vpc" {
-  source         = "git::https://github.com/hvargas2007/vpc-demo.git?ref=v1.0.0"
+  source         = "git::https://github.com/hvargas2007/vpc-demo.git?ref=v1.0.1"
   aws_profile    = var.aws_profile
   aws_region     = var.aws_region
   logs_retention = var.logs_retention
@@ -9,16 +9,4 @@ module "vpc" {
   PrivateSubnet  = var.PrivateSubnet
   name_prefix    = var.name_prefix
   project-tags   = var.project-tags
-}
-
-module "ec2" {
-  source            = "git::https://github.com/hvargas2007/ec2-demo.git?ref=v1.0.0"
-  aws_profile       = var.aws_profile
-  aws_region        = var.aws_region
-  vpc_id            = module.vpc.vpc_id
-  privatesubnet_id = module.vpc.privatesubnet_id
-  key_name          = var.key_name
-  name_prefix       = var.name_prefix
-  project-tags      = var.project-tags
-
 }
