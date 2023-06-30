7b48567dbcaa47e9dc4ea6c48a5534b6d44c930f
vargascloudh <hermes.vargas@cloudhesive.comgit config --global user.name vargascloudh>
2023-06-30 13:50:45 -0300

    profileaws

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 # Create a vpc
 module "vpc" {
   source         = "git::https://github.com/hvargas2007/vpc-demo.git?ref=v1.0.4"
-  aws_profile    = var.aws_profile
   aws_region     = var.aws_region
   logs_retention = var.logs_retention
   vpc_cidr       = var.vpc_cidr
